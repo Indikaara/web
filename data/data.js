e71f0abaf6e9b9ede5efe94dd5807e908a8eef9d
Oshin Batra <oshinbatra07@gmail.com>
2025-07-21 19:07:46 +0530

    listed genuine products & modified theme for shop

---
+++ data/data.js
@@ -0,0 +1,76 @@
+export const data = {
+    products: [
+        {
+            category: "rugs",
+            categoryId: 10000,
+            items: [
+                {
+                    id: 10001,
+                    name: "Apiya",
+                    price: 391230,
+                    priceRange: null,
+                    image: [
+                        "/assets/products/rugs/mirzapur_rugs/handknotted/apiya/1.webp",
+                        "/assets/products/rugs/mirzapur_rugs/handknotted/apiya/2.webp",
+                        "/assets/products/rugs/mirzapur_rugs/handknotted/apiya/3.webp",
+                        "/assets/products/rugs/mirzapur_rugs/handknotted/apiya/4.webp",
+                        "/assets/products/rugs/mirzapur_rugs/handknotted/apiya/5.webp"
+                    ],
+                    story:
+                        "Introducing our exquisite hand-knotted carpets, where craftsmanship meets luxury. Each carpet is meticulously woven by skilled artisans using traditional techniques passed down through generations, resulting in a masterpiece of beauty and quality. Made from premium materials such as wool, silk, and cotton, our hand-knotted carpets boast unparalleled softness, durability, and intricate detail. Whether adorning your living room, bedroom, or office, these carpets add a touch of elegance and sophistication to any space. Elevate your decor with the timeless allure of our hand-knotted carpets, where every thread tells a story of artistry and tradition.",
+                    description:
+                        "Introducing our hand-knotted carpets. Crafted by skilled artisans using traditional techniques, they offer unparalleled softness, durability, and intricate detail. Add elegance to any space with our luxurious carpets.",
+                    dimensionsAvailable: ["9 x 12 ft"],
+                    details: ['Use : Indoor use only', 'Pile Height : 100% Cut', 'Shedding is Inherent to Wool Rugs', 'Suitable for Medium Traffic Areas', 'Good Weave Certified', 'Made in India'],
+                    color: ["Grey-Rust"],
+                    weavingTechnique: "Handwoven",
+                    material: "Wool & Bamboo Silk ",
+                    manufacturer: "Mirzapur Rugs",
+                    tags: ['abstract', 'Modern']
+                }, {
+                    id: 10002,
+                    name: "Ambrosia",
+                    price: 289800,
+                    priceRange: null,
+                    image: [
+                        "/assets/products/rugs/mirzapur_rugs/handknotted/ambrosia/1.webp",
+                        "/assets/products/rugs/mirzapur_rugs/handknotted/ambrosia/2.webp",
+                        "/assets/products/rugs/mirzapur_rugs/handknotted/ambrosia/3.webp"
+                    ],
+                    story:
+                        "Introducing our exquisite hand-knotted carpets, where craftsmanship meets luxury. Each carpet is meticulously woven by skilled artisans using traditional techniques passed down through generations, resulting in a masterpiece of beauty and quality. Made from premium materials such as wool, silk, and cotton, our hand-knotted carpets boast unparalleled softness, durability, and intricate detail. Whether adorning your living room, bedroom, or office, these carpets add a touch of elegance and sophistication to any space. Elevate your decor with the timeless allure of our hand-knotted carpets, where every thread tells a story of artistry and tradition.",
+                    description:
+                        "Introducing our hand-knotted carpets. Crafted by skilled artisans using traditional techniques, they offer unparalleled softness, durability, and intricate detail. Add elegance to any space with our luxurious carpets.",
+                    details: ['Use : Indoor use only', 'Pile Height : 100% Cut', 'Shedding is Inherent to Wool Rugs', 'Suitable for Medium Traffic Areas', 'Good Weave Certified', 'Made in India'],
+                    dimensionsAvailable: ["8 x 10 ft"],
+                    color: ["multi"],
+                    weavingTechnique: "Hand Knotted",
+                    material: "New Zealand Wool",
+                    manufacturer: "Mirzapur Rugs",
+                    tags: ['abstract']
+                },
+                {
+                    id: 10002,
+                    name: "Astara",
+                    price: null,
+                    priceRange: '₹42,525.00 to ₹99,225.00',
+                    image: [
+                        "/assets/products/rugs/mirzapur_rugs/handknotted/astara/1.webp",
+                        "/assets/products/rugs/mirzapur_rugs/handknotted/astara/2.webp"
+                    ],
+                    story:
+                        "Introducing our exquisite hand-knotted carpets, where craftsmanship meets luxury. Each carpet is meticulously woven by skilled artisans using traditional techniques passed down through generations, resulting in a masterpiece of beauty and quality. Made from premium materials such as wool, silk, and cotton, our hand-knotted carpets boast unparalleled softness, durability, and intricate detail. Whether adorning your living room, bedroom, or office, these carpets add a touch of elegance and sophistication to any space. Elevate your decor with the timeless allure of our hand-knotted carpets, where every thread tells a story of artistry and tradition.",
+                    description:
+                        "Introducing our hand-knotted carpets. Crafted by skilled artisans using traditional techniques, they offer unparalleled softness, durability, and intricate detail. Add elegance to any space with our luxurious carpets.",
+                    details: ['Use : Indoor use only', 'Pile Height : 6 - 8 MM 100% Cut', 'Shedding is Inherent to Wool Rugs', 'Suitable for Medium Traffic Areas', 'Good Weave Certified', 'Made in India'],
+                    dimensionsAvailable: ["2.6 x 6 ft", "4 x 6 ft", "5 x 7 ft"],
+                    color: ["grey"],
+                    weavingTechnique: "Hand Knotted",
+                    material: "Wool & Bamboo Silk ",
+                    manufacturer: "Mirzapur Rugs",
+                    tags: ['abstract']
+                },
+            ],
+        },
+    ],
+};
